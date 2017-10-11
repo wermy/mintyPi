1ce2d861591d0bdf5478f82ce0e65e896a790cd7
Unknown <skochw@gmail.com>
2017-10-10 21:11:37 -0500

    Fetching any changes to material theme.

--- update-scripts.sh
+++ update-scripts.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-echo "Updating mintyPi scripts..."
-sleep 6s
+echo "Updating mintyPi..."
+
+echo "Updating material theme..."
+cd /home/pi/.emulationstation/themes/es-theme-material
+git fetch --all
+git reset --hard origin/master
+echo "Done!"
+
+
+
+echo "Done!  Your mintyPi is up-to-date."
+sleep 10s
