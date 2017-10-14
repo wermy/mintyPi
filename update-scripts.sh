4cacc703af3c1f8df44d7fcf9191f0f136c7520d
Unknown <skochw@gmail.com>
2017-10-14 13:43:12 -0500

    Making script update carbon theme.

--- update-scripts.sh
+++ update-scripts.sh
@@ -7,6 +7,12 @@ git fetch --all
 git reset --hard origin/master
 echo "Done!"
 
+echo "Updating carbon theme..."
+cd /home/pi/.emulationstation/themes/es-theme-carbon
+git fetch --all
+git reset --hard origin/master
+echo "Done!"
+
 echo "Updating Will.I.Am's mintyPi theme..."
 cd /home/pi/.emulationstation/themes/tft-mintypi
 git fetch --all
