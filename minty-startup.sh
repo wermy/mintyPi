5016cf7688c8bfaaa8bee04142c70153986c3fec
Unknown <skochw@gmail.com>
2017-10-18 12:41:10 -0500

    Fixing video issue for systems without sound cards.

--- minty-startup.sh
+++ minty-startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 5s
-omxplayer -o alsa -b --layer 10000 "/home/pi/mintyPi/minty-splash.mp4"
-sleep 13s
+omxplayer -o alsa -b --layer 10000 "/home/pi/mintyPi/minty-splash.mp4" &
+sleep 25s
 sudo pkill omxplayer
