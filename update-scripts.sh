b108e44056b78a0bae5a27989d73f2c0edcd7744
Unknown <skochw@gmail.com>
2019-03-03 21:13:16 -0600

    Changing directory back...  :|  Adding some sleeps so I can see what the heck is going on.

--- update-scripts.sh
+++ update-scripts.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 echo "Updating mintyPi..."
 chmod +x /home/pi/mintyPi/minty-startup.sh
+sleep 2s
 
 echo "Updating material theme..."
 cd /home/pi/.emulationstation/themes/es-theme-material
 git fetch --all
 git reset --hard origin/master
 echo "Done!"
+sleep 2s
 
 echo "Updating carbon theme..."
 cd /home/pi/.emulationstation/themes/es-theme-carbon
 git fetch --all
 git reset --hard origin/master
 echo "Done!"
+sleep 2s
 
 echo "Updating Will.I.Am's mintyPi theme..."
 cd /home/pi/.emulationstation/themes/tft-mintypi
 git fetch --all
 git reset --hard origin/master
 echo "Done!"
+sleep 2s
 
 echo "Updating mintyPi scripts..."
 cd /home/pi/OneForAll
 git fetch --all
 git reset --hard origin/mintypi_v3
-cd /home/pi/OneForAll/osd
+cd /home/pi/OneForAll
 sudo -u pi make clean
 sudo -u pi make
 echo "Done!"
+sleep 2s
 
 echo "Your mintyPi is up-to-date.  Rebooting now..."
 sleep 5s
