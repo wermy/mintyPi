a312c92e6cb67c9cd8e39176589135e6109810a2
Unknown <skochw@gmail.com>
2019-04-13 22:30:44 -0500

    Starting monitor_evdev from minty-startup.sh, disabling video for now.

--- minty-startup.sh
+++ minty-startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sleep 5s
-omxplayer -o alsa -b --layer 10000 "/home/pi/mintyPi/minty-splash.mp4" &
-sleep 25s
-sudo pkill omxplayer
+# omxplayer -o alsa -b --layer 10000 "/home/pi/mintyPi/minty-splash.mp4" &
+sudo python /home/pi/OneForAll/monitor_evdev.py
+# sleep 25s
+# sudo pkill omxplayer
