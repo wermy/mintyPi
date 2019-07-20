e930c68c8bb7439f8cc801e8e9135e706ff9567b
Unknown <skochw@gmail.com>
2019-07-20 17:37:46 -0500

    Adding internet connectivity check.

--- update.sh
+++ update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if nc -zw1 google.com 443; then
+  echo "Internet connection OK"
+else
+  echo "No internet detected!  You need to be connected to the internet to run the update script."
+  sleep 5s
+  exit 1
+fi
+
 git fetch --all
 git reset --hard origin/mintyPiv3
 sh ./update-scripts.sh
